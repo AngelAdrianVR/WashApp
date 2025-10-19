c74b6670da27f7d74ae67f1eb08c9537e707e4ec
Angel Vazquez <angelvazquez470@gmail.com>
2025-10-18 20:53:09 -0600

    proyecto pausado. se subio tal y como está

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -14,8 +14,8 @@ defineProps({
 });
 
 const form = useForm({
-    email: '',
-    password: '',
+    email: 'cliente@gmail.com',
+    password: '321321321',
     remember: false,
 });
 
@@ -46,7 +46,7 @@ const submit = () => {
             <div class="relative">
                 <InputLabel for="email" value="Correo Electrónico" class="text-gray-200" />
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pt-6 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+                    <svg class="w-5 h-5 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                 </div>
                 <TextInput
                     id="email"
@@ -64,7 +64,7 @@ const submit = () => {
             <div class="mt-4 relative">
                 <InputLabel for="password" value="Contraseña" class="text-gray-200" />
                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pt-6 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" /></svg>
+                    <svg class="w-5 h-5 text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" /></svg>
                 </div>
                 <TextInput
                     id="password"
@@ -80,7 +80,7 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox v-model:checked="form.remember" name="remember" class="rounded border-gray-500 text-blue-500 focus:ring-blue-600" />
-                    <span class="ms-2 text-sm text-gray-300">Recuérdame</span>
+                    <span class="ms-2 text-sm text-gray-700">Recuérdame</span>
                 </label>
             </div>
 
